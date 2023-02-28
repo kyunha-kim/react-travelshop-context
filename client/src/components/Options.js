e20a9147ab1998cf068e59e4629d7fbaf6288c18
kim-seonkyu <seonkyu93@gmail.com>
2023-03-01 00:16:48 +0900

    done

--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-const Options = ({ name }) => {
+const Options = ({ name, updateItemCount }) => {
   return (
     <form>
-      <input type="checkbox" id={`${name} option`} />{" "}
-      <label htmlFor={`${name} option`}>{name}</label>
+      <input
+        style={{ cursor: "pointer" }}
+        type="checkbox"
+        id={`${name} option`}
+        onChange={(e) => updateItemCount(name, e.target.checked ? 1 : 0)}
+      />{" "}
+      <label style={{ cursor: "pointer" }} htmlFor={`${name} option`}>
+        {name}
+      </label>
     </form>
   );
 };
